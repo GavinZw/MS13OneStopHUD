1e05083f26ffcad440efcce1a04e0bbb041532c4
Gavin <zuowei@senlime.com>
2017-11-26 20:43:02 +0800

    no message

--- MS13OneStopHUD/MS13OneStopHUD.h
+++ MS13OneStopHUD/MS13OneStopHUD.h
@@ -2,15 +2,21 @@
 //  MS13OneStopHUD.h
 //  MS13OneStopHUD
 //
-//  Created by Gavin on 17/11/25.
+//  Created by Gavin on 17/11/26.
 //  Copyright © 2017年 Gavin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#if __has_include(<MS13OneStopHUD/MS13OneStopHUD.h>)
+//! Project version number for MS13OneStopHUD.
 FOUNDATION_EXPORT double MS13OneStopHUDVersionNumber;
+
+//! Project version string for MS13OneStopHUD.
 FOUNDATION_EXPORT const unsigned char MS13OneStopHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MS13OneStopHUD/PublicHeader.h>
+
+#if __has_include(<MS13OneStopHUD/MS13OneStopHUD.h>)
 #import <MS13OneStopHUD/MSAppDelegate.h>
 #import <MS13OneStopHUD/MS13OneStopManage.h>
 #import <MS13OneStopHUD/MSAppDelegate+Service.h>
