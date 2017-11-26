7a2f511b7cf431ee78f2416ed5b6ccb19e7837c7
Gavin <zuowei@senlime.com>
2017-11-26 20:56:19 +0800

    no message

--- MS13OneStopHUD/MS13OneStopHUD.h
+++ MS13OneStopHUD/MS13OneStopHUD.h
@@ -8,15 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for MS13OneStopHUD.
-FOUNDATION_EXPORT double MS13OneStopHUDVersionNumber;
-
-//! Project version string for MS13OneStopHUD.
-FOUNDATION_EXPORT const unsigned char MS13OneStopHUDVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <MS13OneStopHUD/PublicHeader.h>
-
 #if __has_include(<MS13OneStopHUD/MS13OneStopHUD.h>)
+FOUNDATION_EXPORT double MS13OneStopHUDVersionNumber;
+FOUNDATION_EXPORT const unsigned char MS13OneStopHUDVersionString[];
 #import <MS13OneStopHUD/MSAppDelegate.h>
 #import <MS13OneStopHUD/MS13OneStopManage.h>
 #import <MS13OneStopHUD/MSAppDelegate+Service.h>
